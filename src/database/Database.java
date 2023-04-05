e7dd57f0020b5e253c8b1da2c6046698da88d318
User <nuridinovk24@gmail.com>
2023-04-05 17:41:35 +0600

    Coment Hospital class

--- src/database/Database.java
+++ src/database/Database.java
@@ -5,44 +5,46 @@ import model.Doctor;
 import model.Hospital;
 import model.Patient;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Database {
-private Set<Hospital> hospitals = new LinkedHashSet<>();
-private Set<Department> departments= new LinkedHashSet<>();
-private Set<Doctor> doctors = new LinkedHashSet<>();
-private Set<Patient> patients = new LinkedHashSet<>();
+    private List<Hospital> hospitals = new ArrayList<>();
+    private List<Department> departments = new ArrayList<>();
+    private List<Doctor> doctors = new ArrayList<>();
+    private List<Patient> patients = new ArrayList<>();
 
-    public Set<Hospital> getHospitals() {
+    public List<Hospital> getHospitals() {
         return hospitals;
     }
 
-    public void setHospitals(Set<Hospital> hospitals) {
+    public void setHospitals(List<Hospital> hospitals) {
         this.hospitals = hospitals;
     }
 
-    public Set<Department> getDepartments() {
+    public List<Department> getDepartments() {
         return departments;
     }
 
-    public void setDepartments(Set<Department> departments) {
+    public void setDepartments(List<Department> departments) {
         this.departments = departments;
     }
 
-    public Set<Doctor> getDoctors() {
+    public List<Doctor> getDoctors() {
         return doctors;
     }
 
-    public void setDoctors(Set<Doctor> doctors) {
+    public void setDoctors(List<Doctor> doctors) {
         this.doctors = doctors;
     }
 
-    public Set<Patient> getPatients() {
+    public List<Patient> getPatients() {
         return patients;
     }
 
-    public void setPatients(Set<Patient> patients) {
+    public void setPatients(List<Patient> patients) {
         this.patients = patients;
     }
 }
