a7d4ac37b0804a629bcdec43027f9ecb89b76e5e
LiraKanatova <lira.kanaatova.ch@gmail.com>
2023-04-05 14:48:22 +0600

    Added database clacc

---
+++ src/DepartmentService.java
@@ -0,0 +1,11 @@
+import model.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    String addDepartmentToHospital(Long id, Department department);
+    List<Department> getAllDepartmentByHospital(Long id);
+    Department findDepartmentByName(String name);
+    void deleteDepartmentById(Long id);
+    String updateDepartmentById(Long id, Department department);
+}
