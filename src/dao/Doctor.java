384b816b1d07b9bd96699dcde1800b79c15efd9a
Марьям <abdulakimovaaigerim205@gmail.com>
2023-04-05 14:42:50 +0600

    added service Doctor methods

--- src/dao/Doctor.java
+++ src/dao/Doctor.java
@@ -1,4 +1,23 @@
 package dao;
 
+import java.util.List;
+
 public interface Doctor {
+
+    String addDoctorToHospital(Long id, Doctor doctor);
+
+    Doctor findDoctorById(Long id);
+
+    String updateDoctor(Long id, Doctor doctor);
+
+    void deleteDoctorById(Long id);
+
+    // Department‘ти id менен табып, анан Doctor‘лорду листтеги айдилери менен табып анан ошол табылган
+    // Department'ге табылган Doctor'лорду кошуп коюнунуз
+    String assignDoctorToDepartment(Long departmentId, List<Long> doctorsId);
+
+    List<Doctor> getAllDoctorsByHospitalId(Long id);
+
+    List<Doctor> getAllDoctorsByDepartmentId(Long id);
+
 }
--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -68,4 +68,5 @@ public class Doctor {
                 ", experienceYear=" + experienceYear +
                 '}';
     }
+
 }
